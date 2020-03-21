4a4b50cef84ae79894e52b1acb846c17e2cc8c0e
Lucas Didur <ldidur@icloud.com>
2020-03-21 11:18:26 -0300

    fix

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -4,14 +4,14 @@ import "firebase/auth";
 
 let config: { storageBucket: string; apiKey: string; messagingSenderId: string; appId: string; projectId: string; measurementId: string; databaseURL: string; authDomain: string };
 config = {
-    apiKey: "AIzaSyBQxF7N6P-LxzZZ1EgOWoheTedL7sPeKvg",
-    authDomain: "dc-app-produtor-v2.firebaseapp.com",
-    databaseURL: "https://dc-app-produtor-v2.firebaseio.com",
-    projectId: "dc-app-produtor-v2",
-    storageBucket: "dc-app-produtor-v2.appspot.com",
-    messagingSenderId: "614775502350",
-    appId: "1:614775502350:web:7164ae3ed2d57ad044ae5f",
-    measurementId: "G-RWH1JWDW5Y"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
 
 firebase.initializeApp(config);
